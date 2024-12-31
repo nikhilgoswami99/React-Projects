4fd272c3072eea556d59be0f143bb1f242f7ca81
Nikhil Goswami <nikhil.goswami.18121999@gmail.com>
2024-12-31 22:15:44 +0530

    deploying

--- React-Cart/src/App.jsx
+++ React-Cart/src/App.jsx
@@ -1,15 +1,84 @@
+import { useState, useEffect} from "react";
 import styles from "./App.module.css";
 import Navbar from "./components/navbar/nav";
 import Card from "./components/card/card";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      name: "Samsung Galaxy S8",
+      price: 399.99,
+      img_link: "https://www.course-api.com/images/cart/phone-1.png",
+    },
+    {
+      id: 2,
+      name: "Google Pixel",
+      price: 499.99,
+      img_link: "https://www.course-api.com/images/cart/phone-2.png",
+    },
+    {
+      id: 3,
+      name: "Xiaomi Redmi Note 2",
+      price: 699.99,
+      img_link: "https://www.course-api.com/images/cart/phone-3.png",
+    },
+    {
+      id: 4,
+      name: "Samsung Galaxy S7",
+      price: 599.99,
+      img_link: "https://www.course-api.com/images/cart/phone-4.png",
+    },
+  ];
+
+  let [cards, setCards] = useState(data);
+  let [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const newTotalPrice = cards.reduce((acc, obj) => acc + obj.price, 0);
+    setTotalPrice(newTotalPrice);
+  }, []);
+
+  function clearCart()
+  { 
+    setCards([]);
+    setTotalPrice(0);
+  }
+
+  function removeCard(idx) {
+    setCards(
+      cards.filter((obj, index) => {
+        if (idx !== index) {
+          return obj;
+        }
+      })
+    );
+  }
+
   return (
     <>
-    <Navbar></Navbar>
-    <div className={styles.card_container}>
-    <Card></Card>
-    </div>
-    
+      <Navbar></Navbar>
+      <h1 className={styles.cart_heading}>YOUR BAG</h1>
+      <div className={styles.card_container}>
+        {cards.map((obj, index) => {
+          return (
+            <Card
+              key={index}
+              {...obj}
+              idx={index}
+              deleteCard={() => {
+                removeCard(index);
+              }}
+            ></Card>
+          );
+        })}
+      </div>
+
+      <div className={styles.total_items}>
+        <h5 className={styles.total_text}>Total</h5>
+        <button onClick={clearCart} className={styles.clearAll}>Clear Cart</button>
+        <p className={styles.total_amount}>${totalPrice}</p>
+      </div>
     </>
   );
 }
--- React-Cart/src/components/card/card.jsx
+++ React-Cart/src/components/card/card.jsx
@@ -1,12 +1,34 @@
-import styles from './card.module.css'
+import styles from "./card.module.css";
 
-function Card()
-{
-    return <>
-    <div>
-        dklsfgjisdfhignsdfgbsdfg
-    </div>
+function Card(props) {
+// console.log(props);
+
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.item_content}>
+          <img className={styles.item_img} src={props.img_link} alt="" />
+
+          <div className={styles.item_info}>
+            <h5 className={styles.item_name}>{props.name}</h5>
+
+            <span className={styles.item_price}>{props.price}</span>
+
+            <button onClick={props.deleteCard} className={styles.remove_button}>remove</button>
+          </div>
+        </div>
+
+        <div className={styles.item_btns}>
+          <button className={styles.add_btn}>-</button>
+
+          <span className={styles.item_quantity}>1</span>
+
+          <button className={styles.remove_btn}>+</button>
+        </div>
+      </div>
+
     </>
+  );
 }
 
-export default Card
+export default Card;
