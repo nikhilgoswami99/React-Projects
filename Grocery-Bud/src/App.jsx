a52d2529211d558e83679a1f8647e30ae34a6e67
Nikhil Goswami <nikhil.goswami.18121999@gmail.com>
2025-01-02 16:55:46 +0530

    updating

--- Grocery-Bud/src/App.jsx
+++ Grocery-Bud/src/App.jsx
@@ -84,7 +84,7 @@ function App() {
       </div>
       <ToastContainer
         position="top-center"
-        autoClose={3000}
+        autoClose={2000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
